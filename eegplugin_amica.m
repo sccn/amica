ffbcc10fe8527b5e5c662a8af6b622d3ab26d4c6
Arnaud Delorme <arnodelorme@gmail.com>
2021-10-11 13:08:52 -1000

    Update eegplugin_amica.m

--- eegplugin_amica.m
+++ eegplugin_amica.m
@@ -44,7 +44,7 @@
 function vers = eegplugin_amica(fig, trystrs, catchstrs)
     
     %global EEG
-    vers = '1.6';
+    vers = '1.6.1';
     if nargin < 3
         error('eegplugin_amica requires 3 arguments');
     end;
