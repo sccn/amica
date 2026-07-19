632b6287a01063b8a0bb44b1d84e5329c91ec526
Seyed Yahya Shirazi <shirazi@ieee.org>
2026-07-19 07:24:09 -0700

    Wire reproducible random seed from input.param

--- amica15_header.f90
+++ amica15_header.f90
@@ -95,6 +95,9 @@ integer :: num_tot, tot_dim, offset, blk, samp
 INTEGER :: random_init_W = 0, nbyte = 4, read_Winit = 0
 integer :: coststep = 1, writestep = 100, tot_blks, nblks
 INTEGER :: seed(2) = (/ 123456, 654321 /)
+INTEGER :: input_seed = 0, seed_size
+LOGICAL :: use_seed = .false.
+INTEGER, ALLOCATABLE :: seed_put(:)
 INTEGER :: numargs, argnum, filenum, filestart, filestop, sampnum, sampstart
 integer :: sampstop, blknum, blocknum, fld1, fld2, num_blocks
 INTEGER :: i, j, k
